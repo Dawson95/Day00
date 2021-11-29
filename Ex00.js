60054b4c7d757e9a542fa6fee914e04416fe8168
admin <admin@iMac-ITA-02.local>
2021-11-29 10:56:22 +0100

    ex00

---
+++ Ex00.js
@@ -0,0 +1,17 @@
+function twoNumberSum(array, targetSum){
+    for(let i = 0; i < array.length ; i++){
+        for(let y = 1; y < array.length;y++){
+            temp = array[i] + array[y];
+            if(temp == targetSum){
+                return [array[i],array[y]];
+            }
+        }
+  
+  }
+  return [];
+  }
+  
+  console.log(twoNumberSum([1,2,10,8,7,4,9,47],14));
+  
+
+exports.twoNumberSum = twoNumberSum;
